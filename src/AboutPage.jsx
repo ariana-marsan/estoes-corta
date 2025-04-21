a8ee2d11ad8e4a8aad0093b2daca3c43c4c28b7b
ariana-marsan <arimarsan6@gmail.com>
2025-04-21 17:46:52 -0300

    changed information

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -1,5 +1,5 @@
 
-import mati from './images/matias.jpeg';
+import matiynacho from './images/matiasynacho.jpeg';
 
 export function AboutPage() {
     return(
@@ -8,23 +8,23 @@ export function AboutPage() {
             <div className="grid">
             <div className="who">
                 <h1>Quienes somos</h1>
-                <p>Somos un equipo de profesionales dedicados y comprometidos a brindar el mejor servicio posible. Nuestra misión es ofrecer productos y servicios de alta calidad que superen las expectativas de nuestros clientes. Valoramos la integridad, la transparencia y la responsabilidad en todas nuestras interacciones.</p>
+                <p>Somos una agencia de marketing digital en Argentina, creada por dos amigos inquietos y apasionados por emprender, aprender y crecer. Nuestra misión es ayudar a las marcas a comunicar con mayor claridad, estilo y propósito. Lo hacemos a través de contenido estratégico, diseño minimalista y una mirada cercana. En un mundo lleno de ruido, apostamos por el valor de lo simple.</p>
             </div>
             <div className="how">
                 <h1>Cómo lo hacemos</h1>
                 <p>
-                Utilizamos la tecnología y las técnicas más avanzadas para garantizar la excelencia en nuestros servicios.</p>
+                Antes de crear, escuchamos. Analizamos la identidad de tu marca, sus fortalezas, desafíos y objetivos. Con esa base, diseñamos un plan estratégico a medida, con contenido creativo, diseño simple y acciones concretas. Nada al azar. Todo con intención. </p>
             </div>
             <div className="why">
                 <h1>Porqué lo hacemos</h1>
-                <p>Creemos en hacer una diferencia en el mundo y brindar valor a nuestros clientes.</p>
+                <p>Nos inspira la idea de que una marca no necesita exagerar para destacar. Apostamos a un estilo claro y auténtico que conecta. Lo que nos mueve es ayudar a otras personas a crecer, mostrar su valor y dejar una huella con lo que realmente es imprescindible. </p>
             </div>
             <div className="img">
-                <img src={mati} className='imagen-mati'></img>
+                <img src={matiynacho} alt='matias y nacho (fundadores)' className='imagen-mati'></img>
             </div>
             <div className="thanks">
-                <h1>¡Gracias por elegirnos!</h1>
-                <p>Estamos comprometidos a construir relaciones duraderas con nuestros clientes y socios.</p>
+                <h1>Gracias por confiar</h1>
+                <p>Ponemos el corazón en cada proyecto y cuidamos tu marca como propia. Juntos, vamos a crear algo claro, sólido y con propósito.</p>
             </div>
             </div>
         </main>
